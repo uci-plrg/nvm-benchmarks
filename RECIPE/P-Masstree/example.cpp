812a5cd77888d6ca1506994346289014ca383ec2
Hamed Gorjiara <hgorjiar@uci.edu>
2020-08-02 02:24:16 -0700

    Masstree running with PMCheck

--- RECIPE/P-Masstree/example.cpp
+++ RECIPE/P-Masstree/example.cpp
@@ -2,7 +2,6 @@
 #include <chrono>
 #include <random>
 #include <atomic>
-#include <vector>
 #include <thread>
 
 using namespace std;
@@ -10,12 +9,16 @@ using namespace std;
 #include "masstree.h"
 
 extern "C" {
-    void restart();
+    void * getRegionFromID(uint ID);
+    void setRegionFromID(uint ID, void *ptr);
 }
 
 typedef struct thread_data {
     uint32_t id;
     masstree::masstree *tree;
+    uint64_t *keys;
+    uint64_t n;
+    uint num_thread;
 } thread_data_t;
 
 masstree::masstree *tree = NULL;
@@ -34,93 +37,95 @@ void run(char **argv) {
     int num_thread = atoi(argv[2]);
 
     printf("operation,n,ops/s\n");
-    if(tree == NULL){
+    if (getRegionFromID(0) == NULL) {
         masstree::leafnode *init_root = new masstree::leafnode(0);
         tree = new masstree::masstree(init_root);
+        setRegionFromID(0, tree);
+    } else {
+        tree = (masstree::masstree *) getRegionFromID(0);
     }
     thread_data_t *tds = (thread_data_t *) malloc(num_thread * sizeof(thread_data_t));
     std::atomic<int> next_thread_id;
+    next_thread_id.store(0);
+    for (int i = 0; i < num_thread; i++){
+        tds[i].id = next_thread_id.fetch_add(1);
+        tds[i].tree = tree;
+        tds[i].keys = keys;
+        tds[i].n = n;
+        tds[i].num_thread = num_thread;
+    }
     {
         // Build tree
         auto starttime = std::chrono::system_clock::now();
-        next_thread_id.store(0);
-        auto func = [&]() {
-            int thread_id = next_thread_id.fetch_add(1);
-            tds[thread_id].id = thread_id;
-            tds[thread_id].tree = tree;
+        auto func = [](void * arg) -> void * {
+            thread_data_t *tds = (thread_data_t *) arg;
 
-            uint64_t start_key = n / num_thread * (uint64_t)thread_id;
-            uint64_t end_key = start_key + n / num_thread;
+            uint64_t start_key = tds->n / tds->num_thread * (uint64_t)tds->id;
+            uint64_t end_key = start_key + tds->n / tds->num_thread;
             for (uint64_t i = start_key; i < end_key; i++) {
-                tree->put(keys[i], &keys[i]);
+                tree->put(tds->keys[i], &tds->keys[i]);
             }
         };
-        std::vector<std::thread> thread_group;
+        pthread_t *threads = new pthread_t[num_thread];
 
-        for (int i = 0; i < num_thread; i++)
-            thread_group.push_back(std::thread{func});
-
-        for (int i = 0; i < num_thread; i++)
-            thread_group[i].join();
+        for (int i = 0; i < num_thread; i++) {
+            pthread_create(&threads[i], NULL, func, &tds[i]);
+        }
+        for (int i = 0; i < num_thread; i++){
+            pthread_join(threads[i], NULL);
+        }
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
                 std::chrono::system_clock::now() - starttime);
         printf("Throughput: insert,%ld,%f ops/us\n", n, (n * 1.0) / duration.count());
         printf("Elapsed time: insert,%ld,%f sec\n", n, duration.count() / 1000000.0);
+        delete [] threads;
     }
 
     {
         // Lookup
         auto starttime = std::chrono::system_clock::now();
         next_thread_id.store(0);
-        auto func = [&]() {
-            int thread_id = next_thread_id.fetch_add(1);
-            tds[thread_id].id = thread_id;
-            tds[thread_id].tree = tree;
-
-            uint64_t start_key = n / num_thread * (uint64_t)thread_id;
-            uint64_t end_key = start_key + n / num_thread;
+        auto func = [](void * arg) -> void * {
+            thread_data_t *tds = (thread_data_t *) arg;
+            
+            uint64_t start_key = tds->n / tds->num_thread * (uint64_t)tds->id;
+            uint64_t end_key = start_key + tds->n / tds->num_thread;
 
             for (uint64_t i = start_key; i < end_key; i++) {
-                uint64_t *ret = reinterpret_cast<uint64_t *> (tree->get(keys[i]));
-                if (*ret != keys[i]) {
-                    std::cout << "wrong value read: " << *ret << " expected:" << keys[i] << std::endl;
+                uint64_t *ret = reinterpret_cast<uint64_t *> (tree->get(tds->keys[i]));
+                if (*ret != tds->keys[i]) {
+                    std::cout << "wrong value read: " << *ret << " expected:" << tds->keys[i] << std::endl;
                     throw;
                 }
             }
         };
 
-        std::vector<std::thread> thread_group;
+        pthread_t *threads = new pthread_t[num_thread];
 
-        for (int i = 0; i < num_thread; i++)
-            thread_group.push_back(std::thread{func});
-
-        for (int i = 0; i < num_thread; i++)
-            thread_group[i].join();
-            
+        for (int i = 0; i < num_thread; i++) {
+            pthread_create(&threads[i], NULL, func, &tds[i]);
+        }
+        for (int i = 0; i < num_thread; i++){
+            pthread_join(threads[i], NULL);
+        }    
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
                 std::chrono::system_clock::now() - starttime);
         printf("Throughput: lookup,%ld,%f ops/us\n", n, (n * 1.0) / duration.count());
         printf("Elapsed time: lookup,%ld,%f sec\n", n, duration.count() / 1000000.0);
+        delete [] threads;
     }
-
+    delete tds;
     delete[] keys;
 }
-
-char ** argvptr;
 
 int main(int argc, char **argv) {
     if (argc != 3) {
         printf("usage: %s [n] [nthreads]\nn: number of keys (integer)\nnthreads: number of threads (integer)\n", argv[0]);
         return 1;
     }
-    argvptr = argv;
     run(argv);
     if(tree != NULL) {
         delete tree;
     }
     return 0;
-}
-
-void restart(){
-    run(argvptr);
 }
